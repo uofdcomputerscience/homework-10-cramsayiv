6b69cf3a5578e7996795dbb67f2a0ab762ad4410
cramsayiv <37635288+cramsayiv@users.noreply.github.com>
2019-12-18 19:35:29 -0600

    handle errors

--- ResultHomework.playground/Contents.swift
+++ ResultHomework.playground/Contents.swift
@@ -1,5 +1,40 @@
 import Foundation
 
+import Foundation
+
+enum guessingError: Error {
+    case IncorrectSuit
+    case IncorrectNumber
+}
+
+func checkGuess(a: String, b: String, c: String, d: String) -> Result<String, Error> {
+    if (a == c && b == d) {
+        return .success("Correct!")
+    }
+    if (a == c) {
+        return .failure(guessingError.IncorrectSuit)
+    }
+    return .failure(guessingError.IncorrectNumber)
+}
+
+
+let successResult = checkGuess(a: "8", b: "diamonds", c: "8", d: "diamonds")
+switch successResult {
+case .success (let string):
+    print("\(string)")
+case .failure (let error):
+    print("Error: \(error)")
+}
+
+
+let failResult = checkGuess(a: "10", b: "diamonds", c: "8", d: "diamonds")
+switch failResult {
+case .success (let string):
+    print("\(string)")
+case .failure (let error):
+    print("Error: \(error)")
+}
+
 // Implement an Error type. Make sure it has at least two values.
 
 
